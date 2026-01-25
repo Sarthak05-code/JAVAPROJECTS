7bf08b2baa7de843790c4500b562183accad66f3
Sarthak <ytsarthak1@gmail.com>
2026-01-25 21:03:37 +0545

    lol

--- sarthak1/src/App.java
+++ sarthak1/src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) {
         int arr = 0;
         System.out.println(arr);
-        System.out.println("New stuff added");
+        System.out.println("New stuff ad");
     }
 }
