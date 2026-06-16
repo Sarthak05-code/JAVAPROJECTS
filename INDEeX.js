34a88e8586bfbad98f1395b81671339e49a17cd8
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-16 10:52:41 +0545

    work

---
+++ INDEeX.js
@@ -0,0 +1,14 @@
+function caller(num1) {
+    console.log("Hello from function. ");
+
+    if(num1 % 2 == 0) {
+        console.log("This number print even: ");
+        
+    }else {
+        console.log("This number print odd");
+        
+    }
+    
+}
+
+caller(2)
