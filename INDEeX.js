695d1e4c6f7ad2fb5f0a1ec80d24b36b797a4aa9
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-18 12:15:42 +0545

    Refactor product display to use card layout and update HTML structure

--- INDEeX.js
+++ INDEeX.js
@@ -3,19 +3,35 @@ const fetchData = async () => {
     const res = await Data.json()
     const products = res.products
 
-    const tbody = document.querySelector('#dataFetcher tbody')
-    for(let i = 0 ; i < products.length ; ++i) {
-        const row = document.createElement('tr')
-        row.innerHTML = `
-            <td>${products[i].id}</td>
-            <td>${products[i].title}</td>
-            <td>${products[i].price}</td>
-            <td>${products[i].category}</td>
-            <td>${products[i].discountPercentage}</td>
-            <td>${products[i].stock}</td>
+    const wrapper = document.querySelector('.cards-wrapper')
 
-        `
-        tbody.appendChild(row)
+    
+    for (let i = 0; i < products.length; ++i) {
+        const card = document.createElement('div')
+        card.classList.add('card-data')
+
+        const imageRow = document.createElement('img')
+        imageRow.src = products[i].images[0]
+        imageRow.alt = products[i].title
+        imageRow.classList.add('cardImage')
+        card.appendChild(imageRow)
+
+        const nameRow = document.createElement('p')
+        nameRow.classList.add('cardName')
+        nameRow.innerHTML = `${products[i].title}`
+        card.appendChild(nameRow)
+
+        const categoryRow = document.createElement('p')
+        categoryRow.classList.add('cardCategory')
+        categoryRow.innerHTML = `${products[i].category}`
+        card.appendChild(categoryRow)
+
+        const ratingRow = document.createElement('p')
+        ratingRow.classList.add('cardRating')
+        ratingRow.innerHTML = `${products[i].rating}`
+        card.appendChild(ratingRow)
+
+        wrapper.appendChild(card)
     }
 }
 
